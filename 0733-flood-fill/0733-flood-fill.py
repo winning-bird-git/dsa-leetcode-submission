291a41157581b51faec0dcd1b6a5799be6b903b9
winning-bird-git <varsha.cbit@gmail.com>
2026-08-08 18:48:21 +0530

    Time: 0 ms (100%), Space: 19.6 MB (52.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,10 +3,14 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         n=len(image)
         m=len(image[0])
-        visit=image
+        
         tocolor=image[sr][sc]
-        visit[sr][sc]=color
+        image[sr][sc]=color
         q=deque([[sr,sc]])
+        #adding this condition to avoid infinite loop
+        #inpython visit=image doesnt create new arraymaps to same obj
+        if tocolor==color:
+            return image
         while q:
             obj=q.popleft()
             row=obj[0]
@@ -16,10 +20,10 @@ class Solution:
             for i in range(len(rowcheck)):
                 exrow=row+rowcheck[i]
                 excol=col+colcheck[i]
-                if exrow>=0 and exrow<n and excol>=0 and excol<m and image[exrow][excol]== tocolor and visit[exrow][excol]!=color:
+                if exrow>=0 and exrow<n and excol>=0 and excol<m and image[exrow][excol]== tocolor:
+                    image[exrow][excol]=color
                     q.append([exrow,excol])
-                    visit[exrow][excol]=color
-        return visit 
+        return image 
 
 
 
